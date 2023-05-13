15e0191bed48915d2935123c67f5831edf21051e
Elisa Suorsa <84924097+xelmas@users.noreply.github.com>
2023-05-13 12:52:31 +0300

    Add OS check

--- tasks.py
+++ tasks.py
@@ -1,29 +1,32 @@
 from invoke import task
+import os
+
+pty = os.sys.platform.startswith("linux")
 
 @task
 def start(ctx):
-    ctx.run("python3 src/index.py", pty=True)
+    ctx.run("python3 src/index.py", pty=pty)
 
 @task
 def test(ctx):
-    ctx.run("pytest src", pty=True)
+    ctx.run("pytest src", pty=pty)
 
 @task
 def coverage(ctx):
-    ctx.run("coverage run --branch -m pytest src", pty=True)
+    ctx.run("coverage run --branch -m pytest src", pty=pty)
 
 @task(coverage)
 def coverage_report(ctx):
-    ctx.run("coverage html", pty=True)
+    ctx.run("coverage html", pty=pty)
 
 @task
 def format(ctx):
-    ctx.run("autopep8 --in-place --recursive src", pty=True)
+    ctx.run("autopep8 --in-place --recursive src", pty=pty)
 
 @task
 def lint(ctx):
-    ctx.run("pylint src", pty=True)
+    ctx.run("pylint src", pty=pty)
 
 @task
 def build(ctx):
-    ctx.run("python3 src/build.py", pty=True)
+    ctx.run("python3 src/build.py", pty=pty)
